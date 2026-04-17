115c343923c72d402ab7ceda97eb930c5708080f
Chris Wood <ctwoodwa@gmail.com>
2026-04-17 19:29:45 -0400

    feat(ui-adapters-blazor): register Shell namespace and add ShellTests smoke tests

---
+++ packages/ui-adapters-blazor/tests/Components/ShellTests.cs
@@ -0,0 +1,42 @@
+using Sunfish.Components.Blazor.Shell;
+using Xunit;
+
+namespace Sunfish.Components.Blazor.Tests.Components;
+
+public class ShellTests
+{
+    [Fact]
+    public void SunfishAppShell_TypeIsPublicAndInShellNamespace()
+    {
+        var type = typeof(SunfishAppShell);
+        Assert.True(type.IsPublic);
+        Assert.Equal("Sunfish.Components.Blazor.Shell", type.Namespace);
+    }
+
+    [Fact]
+    public void AllExpectedShellTypes_AreInShellNamespace()
+    {
+        var expected = new[]
+        {
+            typeof(SunfishAppShell),
+            typeof(SunfishAppShellNavGroup),
+            typeof(SunfishAppShellNavLink),
+            typeof(SunfishAppShellSlideOver),
+            typeof(SunfishAccountMenu),
+            typeof(SunfishUserMenu),
+            typeof(SunfishNotificationBell),
+            typeof(AccountMenuItemModel),
+            typeof(AccountMenuItemOptions),
+            typeof(AccountMenuOptions),
+            typeof(AppearanceMenuContext),
+            typeof(LanguageMenuContext),
+            typeof(HelpMenuContext),
+            typeof(PopupMenuItem),
+            typeof(NotificationItem),
+        };
+        foreach (var t in expected)
+        {
+            Assert.Equal("Sunfish.Components.Blazor.Shell", t.Namespace);
+        }
+    }
+}
